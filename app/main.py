d1d7e20ae524f25117a392fead79f89e9734608c
Carlos Oliveira <102637302+CarlosOliveira-23@users.noreply.github.com>
2025-01-31 19:50:23 -0300

    :sparkles: feat: Add FastAPI main entrypoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from app.api.endpoints import auth
+from app.api.endpoints import auth, users
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, prefix="/users", tags=["users"])
+
+
+@app.get("/")
+def root():
+    return {"message": "Boilerplate FastAPI funcionando!"}
