1510f32180df6b524ef0bf7124d058de1cafa1d3
Carlos Oliveira <102637302+CarlosOliveira-23@users.noreply.github.com>
2025-01-31 19:49:14 -0300

    :lock: security: Add password hashing and authentication

--- app/core/security.py
+++ app/core/security.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
